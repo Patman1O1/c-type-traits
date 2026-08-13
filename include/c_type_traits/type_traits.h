cc7098a5b02d608d16d68b9a6e4a711d1aa15d2d
Patrick Thomas <patrickthomas101x@gmail.com>
2026-08-13 13:00:07 -0500

    Implement c_type_traits_is_primitive

--- include/c_type_traits/type_traits.h
+++ include/c_type_traits/type_traits.h
@@ -1,12 +1,30 @@
 #ifndef C_TYPE_TRAITS_H
 #define C_TYPE_TRAITS_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif // #ifdef __cplusplus
+// ISO Includes
+#include <complex.h>
+#include <stdio.h>
+#include <stdbool.h>
 
-#ifdef __cplusplus
-}
-#endif // #ifdef __cplusplus
+#define c_type_traits_is_primitive(type) _Generic((typeof_unqual(type)){0}, \
+    bool:                true, \
+    char:                true, \
+    unsigned char:       true, \
+    signed char:         true, \
+    short int:           true, \
+    unsigned short int:  true, \
+    int:                 true, \
+    unsigned int:        true, \
+    long int:            true, \
+    unsigned long int:   true, \
+    float:               true, \
+    float complex:       true, \
+    double:              true, \
+    double complex:      true, \
+    long double:         true, \
+    long double complex: true, \
+    default:             false \
+)
 
 #endif // #ifndef C_TYPE_TRAITS_H
+
