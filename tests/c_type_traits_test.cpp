f71e6ae27a36db856d36d33a5a2f27c5896c269b
Patrick Thomas <patrickthomas101x@gmail.com>
2026-08-13 21:47:31 -0500

    Rename test functions

--- tests/c_type_traits_test.cpp
+++ tests/c_type_traits_test.cpp
@@ -15,14 +15,14 @@ namespace c_type_traits_testing {
     } // namespace
 
     TEST(is_integral, int_types) {
-        EXPECT_TRUE(c_type_traits_tests_is_integral_ints());
+        EXPECT_TRUE(c_type_traits_testing_is_integral_ints());
     }
 
     TEST(is_integral, non_int_types) {
-        EXPECT_TRUE(c_type_traits_tests_is_integral_non_ints());
+        EXPECT_TRUE(c_type_traits_testing_is_integral_non_ints());
     }
 
     TEST(is_integral, alias_int_types) {
-        EXPECT_TRUE(c_type_traits_tests_is_integral_alias_ints());
+        EXPECT_TRUE(c_type_traits_testing_is_integral_alias_ints());
     }
 } // namespace c_type_traits_testing
