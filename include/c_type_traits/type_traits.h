57b9119a2e6d1b46c18fef7af4cb0d55f8620d43
Patrick Thomas <patrickthomas101x@gmail.com>
2026-08-13 21:37:24 -0500

    Remove c_type_traits_is_primitive

--- include/c_type_traits/type_traits.h
+++ include/c_type_traits/type_traits.h
@@ -14,25 +14,6 @@
 
 #define c_type_traits_is_pointer(type) (__builtin_classify_type(type) == 5)
 
-#define c_type_traits_is_primitive(type) _Generic((typeof_unqual(type)){0}, \
-    bool:                true, \
-    char:                true, \
-    unsigned char:       true, \
-    signed char:         true, \
-    short int:           true, \
-    unsigned short int:  true, \
-    int:                 true, \
-    unsigned int:        true, \
-    long int:            true, \
-    unsigned long int:   true, \
-    float:               true, \
-    float complex:       true, \
-    double:              true, \
-    double complex:      true, \
-    long double:         true, \
-    long double complex: true, \
-    default:             false \
-)
 
 #endif // #ifndef C_TYPE_TRAITS_H
 
