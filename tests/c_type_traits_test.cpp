d79a666a77893665df5d52f32c224af676cdc9f9
Patrick Thomas <patrickthomas101x@gmail.com>
2026-08-13 21:38:33 -0500

    Implement tests for is_integral

--- tests/c_type_traits_test.cpp
+++ tests/c_type_traits_test.cpp
@@ -1,10 +1,24 @@
+// ISO C++ Includes
+#include <type_traits>
+
 // Google Test Includes
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
 // Local Includes
-#include <c_type_traits/c_type_traits.h>
+#include <c_type_traits/type_traits.h>
+#include "include/c_type_traits_test.h"
 
 namespace c_type_traits_testing {
+    namespace {
 
+    } // namespace
+
+    TEST(is_integral, int_types) {
+        EXPECT_TRUE(c_type_traits_tests_is_integral_ints());
+    }
+
+    TEST(is_integral, non_int_types) {
+        EXPECT_TRUE(c_type_traits_tests_is_integral_non_ints());
+    }
 } // namespace c_type_traits_testing
