a0bb9a7ce132f1a1be8ec1f6b49296f14b8460c9
Patrick Thomas <patrickthomas101x@gmail.com>
2026-08-13 21:37:42 -0500

    Add tests/include

---
+++ tests/include/c_type_traits_test.h
@@ -0,0 +1,21 @@
+#ifndef C_TYPE_TRAITS_TEST_H
+#define C_TYPE_TRAITS_TEST_H
+
+#include <stdbool.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif // #ifdef __cplusplus
+
+extern bool c_type_traits_tests_is_integral_ints(void);
+
+extern bool c_type_traits_tests_is_integral_non_ints(void);
+
+extern bool c_type_traits_tests_is_integral_alias_ints(void);
+
+#ifdef __cplusplus
+}
+#endif // #ifdef __cplusplus
+
+#endif // #ifndef C_TYPE_TRAITS_TEST_H
+
