d9dad8da110a214e233bd2db11c3056382776bad
Patrick Thomas <patrickthomas101x@gmail.com>
2026-08-13 21:46:31 -0500

    Rename test functions

--- tests/include/c_type_traits_test.h
+++ tests/include/c_type_traits_test.h
@@ -7,11 +7,12 @@
 extern "C" {
 #endif // #ifdef __cplusplus
 
-extern bool c_type_traits_tests_is_integral_ints(void);
+extern bool c_type_traits_testing_is_integral_ints(void);
 
-extern bool c_type_traits_tests_is_integral_non_ints(void);
+extern bool c_type_traits_testing_is_integral_non_ints(void);
 
-extern bool c_type_traits_tests_is_integral_alias_ints(void);
+extern bool c_type_traits_testing_is_integral_alias_ints(void);
+
 
 #ifdef __cplusplus
 }
