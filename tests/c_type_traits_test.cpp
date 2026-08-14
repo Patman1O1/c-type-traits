ddac138e4cebcee4e179bb5c886f136b043a2c95
Patrick Thomas <patrickthomas101x@gmail.com>
2026-08-13 21:41:31 -0500

    Add alias test

--- tests/c_type_traits_test.cpp
+++ tests/c_type_traits_test.cpp
@@ -21,4 +21,8 @@ namespace c_type_traits_testing {
     TEST(is_integral, non_int_types) {
         EXPECT_TRUE(c_type_traits_tests_is_integral_non_ints());
     }
+
+    TEST(is_integral, alias_int_types) {
+        EXPECT_TRUE(c_type_traits_tests_is_integral_alias_ints());
+    }
 } // namespace c_type_traits_testing
