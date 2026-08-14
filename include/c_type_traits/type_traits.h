188332970753800dbebdd300ff114c8cc2617ae0
Patrick Thomas <patrickthomas101x@gmail.com>
2026-08-13 21:33:05 -0500

    Remove prv.h

--- include/c_type_traits/type_traits.h
+++ include/c_type_traits/type_traits.h
@@ -1,10 +1,18 @@
 #ifndef C_TYPE_TRAITS_H
 #define C_TYPE_TRAITS_H
 
+#ifndef __GNUC__
+#error "Only GCC and Clang compiler toolchains are allowed."
+#endif // #ifndef __GNUC__
+
 // ISO Includes
 #include <complex.h>
 #include <stdio.h>
 #include <stdbool.h>
+
+#define c_type_traits_is_integral(type) (__builtin_classify_type(type) == 1)
+
+#define c_type_traits_is_pointer(type) (__builtin_classify_type(type) == 5)
 
 #define c_type_traits_is_primitive(type) _Generic((typeof_unqual(type)){0}, \
     bool:                true, \
