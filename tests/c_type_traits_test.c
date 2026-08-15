4ceb3daa79b934151941ce9e5da17e2b12447f99
Patrick Thomas <patrickthomas101x@gmail.com>
2026-08-14 22:47:52 -0500

    Implement tests

--- tests/c_type_traits_test.c
+++ tests/c_type_traits_test.c
@@ -1,6 +1,7 @@
 #define _GNU_SOURCE
 
 // ISO Includes
+#include <complex.h>
 #include <assert.h>
 #include <stdbool.h>
 #include <stddef.h>
@@ -16,11 +17,23 @@ enum test_enum { x, y };
 
 union test_union { int x; int y; };
 
+typedef struct test_struct test_struct_typedef;
+
+typedef enum test_enum test_enum_typedef;
+
+typedef union test_union test_union_typedef;
+
 typedef float float32_t;
 
 typedef double float64_t;
 
 typedef long double float128_t;
+
+typedef float complex complex32_t;
+
+typedef double complex complex64_t;
+
+typedef long double complex complex128_t;
 
 bool c_type_traits_testing_is_int_ints(void) {
     static_assert(c_type_traits_is_int(char));
@@ -102,5 +115,122 @@ bool c_type_traits_testing_is_float_alias_floats(void) {
     static_assert(c_type_traits_is_float(float64_t));
     static_assert(c_type_traits_is_float(float128_t));
     
+    return true;
+}
+
+bool c_type_traits_testing_is_complex_complex(void) {
+    static_assert(c_type_traits_is_complex(float complex));
+    static_assert(c_type_traits_is_complex(double complex));
+    static_assert(c_type_traits_is_complex(long double complex));
+
+    return true;
+}
+
+bool c_type_traits_testing_is_complex_non_complex(void) {
+    static_assert(!c_type_traits_is_complex(float));
+    static_assert(!c_type_traits_is_complex(double));
+    static_assert(!c_type_traits_is_complex(long double));
+    static_assert(!c_type_traits_is_complex(int));
+    static_assert(!c_type_traits_is_complex(char));
+
+    return true;
+}
+
+bool c_type_traits_testing_is_bool_bool(void) {
+    static_assert(c_type_traits_is_bool(bool));
+
+    return true;
+}
+
+bool c_type_traits_testing_is_bool_non_bool(void) {
+    static_assert(!c_type_traits_is_bool(int));
+    static_assert(!c_type_traits_is_bool(float));
+    static_assert(!c_type_traits_is_bool(char));
+
+    return true;
+}
+
+bool c_type_traits_testing_is_struct_struct(void) {
+    static_assert(c_type_traits_is_struct(struct test_struct));
+
+    return true;
+}
+
+bool c_type_traits_testing_is_struct_non_struct(void) {
+    static_assert(!c_type_traits_is_struct(union test_union));
+    static_assert(!c_type_traits_is_struct(enum test_enum));
+    static_assert(!c_type_traits_is_struct(int));
+    static_assert(!c_type_traits_is_struct(char));
+    static_assert(!c_type_traits_is_struct(float));
+
+    return true;
+}
+
+bool c_type_traits_testing_is_struct_alias_struct(void) {
+    static_assert(c_type_traits_is_struct(test_struct_typedef));
+
+    return true;
+}
+
+bool c_type_traits_testing_is_enum_enum(void) {
+    static_assert(c_type_traits_is_enum(enum test_enum));
+
+    return true;
+}
+
+bool c_type_traits_testing_is_enum_non_enum(void) {
+    static_assert(!c_type_traits_is_enum(struct test_struct));
+    static_assert(!c_type_traits_is_enum(union test_union));
+    static_assert(!c_type_traits_is_enum(int));
+    static_assert(!c_type_traits_is_enum(float));
+    static_assert(!c_type_traits_is_enum(char));
+    static_assert(!c_type_traits_is_enum(void*));
+
+    return true;
+}
+
+bool c_type_traits_testing_is_enum_alias_enum(void) {
+    static_assert(c_type_traits_is_enum(test_enum_typedef));
+
+    return true;
+}
+
+bool c_type_traits_testing_is_union_union(void) {
+    static_assert(c_type_traits_is_union(union test_union));
+
+    return true;
+}
+
+bool c_type_traits_testing_is_union_non_union(void) {
+    static_assert(!c_type_traits_is_union(struct test_struct));
+    static_assert(!c_type_traits_is_union(enum test_enum));
+    static_assert(!c_type_traits_is_union(int));
+    static_assert(!c_type_traits_is_union(char));
+    static_assert(!c_type_traits_is_union(float));
+    static_assert(!c_type_traits_is_union(void*));
+
+    return true;
+}
+
+bool c_type_traits_testing_is_union_alias_union(void) {
+    static_assert(c_type_traits_is_union(test_union_typedef));
+
+    return true;
+}
+
+bool c_type_traits_testing_is_array_array(void) {
+    static_assert(c_type_traits_is_array(int[]));
+    static_assert(c_type_traits_is_array(char[]));
+    static_assert(c_type_traits_is_array(float[]));
+
+    return true;
+}
+
+bool c_type_traits_testing_is_array_non_array(void) {
+    static_assert(!c_type_traits_is_array(void*));
+    static_assert(!c_type_traits_is_array(int));
+    static_assert(!c_type_traits_is_array(float));
+    static_assert(!c_type_traits_is_array(char));
+
     return true;
 }
