7c12720b769ecf06bdf0fe112387ffe9fabe6660
Patrick Thomas <patrickthomas101x@gmail.com>
2026-08-14 22:47:15 -0500

    Implement tests for new c_type_trait macros

--- tests/include/c_type_traits_test.h
+++ tests/include/c_type_traits_test.h
@@ -23,6 +23,39 @@ extern bool c_type_traits_testing_is_float_non_floats(void);
 
 extern bool c_type_traits_testing_is_float_alias_floats(void);
 
+extern bool c_type_traits_testing_is_complex_complex(void);
+
+extern bool c_type_traits_testing_is_complex_non_complex(void);
+
+extern bool c_type_traits_testing_is_complex_alias_complex(void);
+
+extern bool c_type_traits_testing_is_struct_struct(void);
+
+extern bool c_type_traits_testing_is_struct_non_struct(void);
+
+extern bool c_type_traits_testing_is_struct_alias_struct(void);
+
+extern bool c_type_traits_testing_is_enum_enum(void);
+
+extern bool c_type_traits_testing_is_enum_non_enum(void);
+
+extern bool c_type_traits_testing_is_enum_alias_enum(void);
+
+extern bool c_type_traits_testing_is_union_union(void);
+
+extern bool c_type_traits_testing_is_union_non_union(void);
+
+extern bool c_type_traits_testing_is_union_alias_union(void);
+
+extern bool c_type_traits_testing_is_array_array(void);
+
+extern bool c_type_traits_testing_is_array_non_array(void);
+
+extern bool c_type_traits_testing_is_function_function(void);
+
+extern bool c_type_traits_testing_is_function_non_function(void);
+
+
 #ifdef __cplusplus
 }
 #endif // #ifdef __cplusplus
