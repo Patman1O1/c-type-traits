c4730401294cd747e49c4bcdc137e50f7c039950
Patrick Thomas <patrickthomas101x@gmail.com>
2026-08-15 15:35:34 -0500

    Rename tests and declare const and volatile tests

--- tests/include/c_type_traits_test.h
+++ tests/include/c_type_traits_test.h
@@ -7,57 +7,45 @@
 extern "C" {
 #endif // #ifdef __cplusplus
 
-extern bool c_type_traits_testing_is_int_ints(void);
+extern bool c_type_traits_testing_is_int_true_cases(void);
 
-extern bool c_type_traits_testing_is_int_non_ints(void);
+extern bool c_type_traits_testing_is_int_false_cases(void);
 
-extern bool c_type_traits_testing_is_int_alias_ints(void);
+extern bool c_type_traits_testing_is_ptr_true_cases(void);
 
-extern bool c_type_traits_testing_is_ptr_ptr_types(void);
+extern bool c_type_traits_testing_is_ptr_false_cases(void);
 
-extern bool c_type_traits_testing_is_ptr_non_ptr_types(void);
+extern bool c_type_traits_testing_is_float_true_cases(void);
 
-extern bool c_type_traits_testing_is_float_floats(void);
+extern bool c_type_traits_testing_is_float_false_cases(void);
 
-extern bool c_type_traits_testing_is_float_non_floats(void);
+extern bool c_type_traits_testing_is_complex_true_cases(void);
 
-extern bool c_type_traits_testing_is_float_alias_floats(void);
+extern bool c_type_traits_testing_is_complex_false_cases(void);
 
-extern bool c_type_traits_testing_is_complex_complex(void);
+extern bool c_type_traits_testing_is_struct_true_cases(void);
 
-extern bool c_type_traits_testing_is_complex_non_complex(void);
+extern bool c_type_traits_testing_is_struct_false_cases(void);
 
-extern bool c_type_traits_testing_is_complex_alias_complex(void);
+extern bool c_type_traits_testing_is_enum_true_cases(void);
 
-extern bool c_type_traits_testing_is_struct_struct(void);
+extern bool c_type_traits_testing_is_enum_false_cases(void);
 
-extern bool c_type_traits_testing_is_struct_non_struct(void);
+extern bool c_type_traits_testing_is_union_true_cases(void);
 
-extern bool c_type_traits_testing_is_struct_alias_struct(void);
+extern bool c_type_traits_testing_is_union_false_cases(void);
 
-extern bool c_type_traits_testing_is_enum_enum(void);
+extern bool c_type_traits_testing_is_array_true_cases(void);
 
-extern bool c_type_traits_testing_is_enum_non_enum(void);
+extern bool c_type_traits_testing_is_array_false_cases(void);
 
-extern bool c_type_traits_testing_is_enum_alias_enum(void);
+extern bool c_type_traits_testing_is_const_true_cases(void);
 
-extern bool c_type_traits_testing_is_union_union(void);
+extern bool c_type_traits_testing_is_const_false_cases(void);
 
-extern bool c_type_traits_testing_is_union_non_union(void);
+extern bool c_type_traits_testing_is_volatile_true_cases(void);
 
-extern bool c_type_traits_testing_is_union_alias_union(void);
-
-extern bool c_type_traits_testing_is_array_array(void);
-
-extern bool c_type_traits_testing_is_array_non_array(void);
-
-extern bool c_type_traits_testing_is_const_const(void);
-
-extern bool c_type_traits_testing_is_const_non_const(void);
-
-extern bool c_type_traits_testing_is_volatile_volatile(void);
-
-extern bool c_type_traits_testing_is_volatile_non_volatile(void);
+extern bool c_type_traits_testing_is_volatile_false_cases(void);
 
 #ifdef __cplusplus
 }
