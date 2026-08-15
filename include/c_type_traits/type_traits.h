a798940c117e256cb1acb07f2e2835a9f3483439
Patrick Thomas <patrickthomas101x@gmail.com>
2026-08-14 22:46:05 -0500

    Implement is_complex, is_bool, is_struct, is_enum, is_union, and is_array

--- include/c_type_traits/type_traits.h
+++ include/c_type_traits/type_traits.h
@@ -19,5 +19,24 @@
 #define c_type_traits_is_float(type) \
     (__builtin_classify_type(type) == 8)
 
+#define c_type_traits_is_complex(type) \
+    (__builtin_classify_type(type) == 9)
+
+#define c_type_traits_is_bool(type) \
+    (__builtin_classify_type(type) == 4)
+
+#define c_type_traits_is_struct(type) \
+    (__builtin_classify_type(type) == 12)
+
+#define c_type_traits_is_enum(type) \
+    (__builtin_classify_type(type) == 3)
+
+#define c_type_traits_is_union(type) \
+    (__builtin_classify_type(type) == 13)
+
+#define c_type_traits_is_array(type) \
+    (__builtin_classify_type(type) == 14)
+
+
 #endif // #ifndef C_TYPE_TRAITS_H
 
